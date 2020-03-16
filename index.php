36c3bd721206a9098ff02387d2e09118e06f8685
tools.commons-poty <tools.commons-poty@tools-sgebastion-07.tools.eqiad.wmflabs>
2020-03-16 12:31:25 +0000

    move

--- index.php
+++
@@ -1,4 +0,0 @@
-<?php
-header("Location: https://commons.wikimedia.org/wiki/Commons:Picture_of_the_Year");
-?>
-Redirecting...<br><a href="https://commons.wikimedia.org/wiki/Commons:Picture_of_the_Year">Please click here if you are not redirected within a few seconds</a>
