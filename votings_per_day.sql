0047c1f804496f6b86d482b8edc8880b607c8c4c
Steinsplitter <steinsplitter-wiki@live.com>
2020-03-16 12:02:25 +0100

    fix

--- votings_per_day.sql
+++ votings_per_day.sql
@@ -1,7 +1,7 @@
 USE commonswiki_p;
 SELECT
   DATE(CONCAT(YEAR(rc_timestamp),"-",MONTH(rc_timestamp),"-",DAY(rc_timestamp))) AS day,
-  COUNT(DISTINCT rc_user) AS POTY_users
+  COUNT(rc_id) AS potyedits
 FROM recentchanges_compat
 WHERE rc_comment LIKE "%POTY vote - eligible on%"
 GROUP BY day;
